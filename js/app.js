12e222b5b3239a756574a1ed12693f2375a7a5c6
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-08 23:44:22 +0900

    Feat : 댓글 정보 출력

--- js/app.js
+++ js/app.js
@@ -94,6 +94,7 @@ function newsFeed() {
 function newsDetail() {
   const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id));
+
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -120,7 +121,32 @@ function newsDetail() {
     </div>
   `;
 
-  container.innerHTML = template;
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for (let i = 0; i < comments.length; i++) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>
+        `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  container.innerHTML = template.replace(
+    '{{__comments__}}',
+    makeComment(newsContent.comments)
+  );
 }
 
 function router() {
